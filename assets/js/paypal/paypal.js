9b953124b3ab683f7b44797b47d25e334978e863
rauljr7 <doyouguysspam@gmail.com>
2025-09-22 15:22:13 -0700

    s

--- assets/js/paypal/paypal.js
+++ assets/js/paypal/paypal.js
@@ -45,8 +45,10 @@ async function setupPayPalButton(paypalPaymentSession) {
     document.addEventListener("click", async (event) => {
         if (event.target.tagName === "PAYPAL-BUTTON") {
             if (get_session_basket_purchase_units_items().length < 1) {
-            console.log("less than 1");
-                document.getElementById("add_to_cart_btn").click();
+                update_session_from_ui(current_product_object).then(function () {
+                update_add_to_cart_cta_based_on_cart();
+                redirect_to_cart();
+                });
             }
         const createOrderIdPromiseReference = createOrder();
         try {
