9228698cf3b19e7c0faa5be9b34efeee307e7bf4
rauljr7 <doyouguysspam@gmail.com>
2025-09-24 12:31:35 -0700

    s

--- assets/js/paypal/messages.js
+++ assets/js/paypal/messages.js
@@ -7,8 +7,10 @@ async function initMessages() {
 
   // Optionally, set initial amount from session on first load (PayPal has autoload already)
   try {
-    let initial_amount = get_session_basket_purchase_units()[0].amount.value;
-    setMessagesAmount(initial_amount);
+    let total_amount = get_session_basket_purchase_units()[0].amount.value;
+    let unit_amount = get_session_basket_purchase_units()[0].items[0].unit_amount.value;
+    let notify_payload = { total_amount: total_amount, unit_amount: unit_amount };
+    setMessagesAmount(notify_payload);
   } catch {}
 }
 
